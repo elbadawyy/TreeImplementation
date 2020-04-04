6ca86ce819be9e78951646ff5d3fa1432db17386
Muhammad El-Badawy <muhammadelbadawy10@gmail.com>
2020-04-04 22:33:00 +0200

    fix indentation

--- Tree.py
+++ Tree.py
@@ -3,20 +3,22 @@ from Node import Node
 
 class Tree:
 	root = None
+	
 
+	
 	def insert(self, data):
 		new_node = Node(data)
 
-	if self.root == None:
-		self.root = new_node
-	else:
-		self.__insert_a_not_root_node(self.root,new_node)
+		if self.root == None:
+			self.root = new_node
+		else:
+			self.__insert_a_not_root_node(self.root,new_node)
 
 	def __insert_a_not_root_node(self,current_node ,new_node):
-		if current_node.right == None && current_node.left != None:
+		if current_node.right == None and current_node.left != None:
 			current_node.right = new_node
 			
-		elif current_node.right == None && current_node.left == None:
+		elif current_node.right == None and current_node.left == None:
 			current_node.left = new_node
 		
 
