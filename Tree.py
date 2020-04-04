b4db7df7abfbbd8482df8733c31947b9c601959c
Muhammad El-Badawy <muhammadelbadawy10@gmail.com>
2020-04-04 22:03:50 +0200

    update

--- Tree.py
+++ Tree.py
@@ -5,5 +5,20 @@ class Tree:
 	root = None
 
 	def insert(self, data):
-		if self.root == None:
-			new_node = Node(data)
+		new_node = Node(data)
+
+	if self.root == None:
+		self.root = new_node
+	else:
+		self.__insert_a_not_root_node(self.root,new_node)
+
+	def __insert_a_not_root_node(self,current_node ,new_node):
+		if current_node.right == None && current_node.left != None:
+			current_node.right = new_node
+			
+		elif current_node.right == None && current_node.left == None:
+			current_node.left = new_node
+		
+
+
+
